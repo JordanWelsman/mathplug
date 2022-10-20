11c1579289e2f932ead027add0f0a8cb9986caf4
Jordan Welsman <jordan.welsman@hotmail.co.uk>
2022-10-20 00:07:02 -0500

    Forced package builder to read description from README.md. Pre-build commit.

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name = 'mathplug',
     version = '0.0.1',
     description = 'A lightweight Python package which supplies simple math functions.',
-    long_description = 'mathplug is a lightweight Python package which supplies simple math functions. It was primarily designed as an excuse to learn how to publish a Python package. Secondarily, this will give me an opportunity to learn some of the more complicated mathematical equations since I somehow skipped that part. In the future, you\'ll be able to install and import this software into your notebook or Python file and use it to add some numbers together. How exciting!',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
     author = 'Jordan Welsman',
     author_email = 'jordan.welsman@outlook.com',
     url = 'https://github.com/JordanWelsman/mathplug/',
